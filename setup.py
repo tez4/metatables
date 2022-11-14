405cfbdcc6c85da2e06faee8c5c4f37ae992b084
tez4 <joel.twann@bluewin.ch>
2022-11-14 09:22:50 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.9'
+DESCRIPTION = 'Data wrangling on files read with pyreadstat'
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Setting up
+setup(
+    name="metatables",
+    version=VERSION,
+    author="Joël Grosjean",
+    author_email="<joel.grosjean@gfs-zh.ch>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/tez4/metatables",
+    packages=find_packages(),
+    install_requires=[
+        "pyreadstat >= 1.1",
+        "pandas >= 1.3"
+    ],
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+            "check-manifest",
+        ]
+    },
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
