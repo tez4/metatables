70326092fca4ce2d6d8d61a4dff807af7971a8d3
tez4 <joel.twann@bluewin.ch>
2022-11-14 10:40:04 +0100

    Added documentation of object

--- metatables/main.py
+++ metatables/main.py
@@ -5,10 +5,21 @@ import pandas as pd
 
 class MetaTable:
     def __init__(self, df: pd.DataFrame, meta: pyreadstat._readstat_parser.metadata_container) -> None:
+        """creates MetaTable object that makes data wrangling of files read with pyreadstat easy
+
+        Args:
+            df (pd.DataFrame): DataFrame read with pyreadstat
+            meta (pyreadstat._readstat_parser.metadata_container): pyreadstat metadata
+        """
         self.df = df
         self.meta = meta
 
     def select_columns(self, columns: list):
+        """selects the given columns and removes all others.
+
+        Args:
+            columns (list): Columns to select
+        """
         column_indexes = [i for i, v in enumerate(self.meta.column_names) if v in columns]
         column_labels = [v for i, v in enumerate(self.meta.column_labels) if i in column_indexes]
 
@@ -39,8 +50,19 @@ class MetaTable:
         self.meta.number_columns = len(columns)
         self.df = self.df[columns]
 
-    def val_lab(self, columns: list[str], labels: dict[int, str]):
+    def val_lab(self, columns: list[str], labels: dict[int, str] | str):
+        """Changes the value labels of the given columns
+
+        Args:
+            columns (list[str]): A list of columns that need new value labels
+            labels (dict[int, str] | str): A dictionary with new labels \
+{1 : "label for code 1", 2: "label for code 2"} or the variable name with the labels to be used "variable_name"
+        """
         # my_indexes = [i for i, v in enumerate(self.meta.column_names) if v in columns]
+
+        # if string use labels of other variables, else use the given labels
+        if isinstance(labels, str):
+            labels = self.meta.variable_value_labels[labels]
 
         # find correct label name
         used = set([int(re.findall(r'\d+', v)[-1]) for v in list(self.meta.variable_to_label.values())])
@@ -57,6 +79,12 @@ class MetaTable:
         self.meta.value_labels[label_name] = labels
 
     def var_lab(self, columns: list[str], text: str):
+        """Changes the variable labels of the given columns
+
+        Args:
+            columns (list[str]): A list of columns that need a new variable label
+            text (str): Text of the variable label
+        """
         my_indexes = [i for i, v in enumerate(self.meta.column_names) if v in columns]
 
         for column in columns:
@@ -65,7 +93,12 @@ class MetaTable:
         for i in my_indexes:
             self.meta.column_labels[i] = text
 
-    def show(self, variable):
+    def show(self, variable: str):
+        """Shows info about the value labels and variable label of the given variable
+
+        Args:
+            variable (str): Variable to be shown
+        """
         print(self.meta.column_names_to_labels[variable])
 
         print('')
@@ -78,6 +111,12 @@ class MetaTable:
         print(list(value_labels.values()))
 
     def write_sav(self, path: str):
+        """Writes a sav file of the MetaTable object which includes a Dataframe and the metadata.
+
+        Args:
+            path (str): Path of the new file, includes the file name. To save the file in the package directory use \
+"./filename.sav"
+        """
         pyreadstat.write_sav(
             df=self.df,
             dst_path=path,
@@ -94,8 +133,12 @@ if __name__ == '__main__':
     print(meta1.variable_value_labels['F5_01'])
 
     x = MetaTable(df1, meta1)
+
+    x.val_lab(['F5_03', 'F5_04'], 'F1_01')
+
     x.val_lab(['F5_01', 'F5_02'], {1: 'Fish', 2: 'Chips'})
     x.var_lab(['F5_01', 'F5_02'], 'New Label')
-    x.select_columns(['F5_01', 'F5_02'])
+
+    x.select_columns(['F5_01', 'F5_02', 'F5_03', 'F5_04'])
     x.show('F5_01')
     x.write_sav('./data/fertig_test.sav')
